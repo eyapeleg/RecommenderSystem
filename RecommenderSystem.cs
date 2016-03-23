909a08ca9781ce8097c7ebf8828557fe7be49f2d
eyapeleg <eyal.peleg@intel.com>
2016-03-23 11:42:21 +0200

    init repo

---
+++ RecommenderSystem.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RecommenderSystem
+{
+    class RecommenderSystem
+    {
+        public enum PredictionMethod { Pearson, Cosine, Random };
+
+        //class members here
+
+        //constructor
+        public RecommenderSystem()
+        {
+        }
+
+        //load a datatset 
+        //The file contains one row for each u,i rating, in the following format:
+        //userid::itemid::rating::timestamp
+        //More at http://recsyswiki.com/wiki/Movietweetings
+        //Download at https://github.com/sidooms/MovieTweetings/tree/master/latest
+        //Do all precomputations here if needed
+        public void Load(string sFileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        //return a list of the ids of all the users in the dataset
+        public List<string> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        //returns a list of all the items in the dataset
+        public List<string> GetAllItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        //returns the list of all items that the given user has rated in the dataset
+        public List<string> GetRatedItems(string sUID)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Returns a user-item rating that appears in the dataset (not predicted)
+        public double GetRating(string sUID, string sIID)
+        {
+            throw new NotImplementedException();
+        }
+
+        //predict a rating for a user item pair using the specified method
+        public double PredictRating(PredictionMethod m, string sUID, string sIID)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Compute MAE (mean absolute error) for a set of rating prediction methods over the same user-item pairs
+        //cTrials specifies the number of user-item pairs to be tested
+        public Dictionary<PredictionMethod, double> ComputeMAE(List<PredictionMethod> lMethods, int cTrials)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
