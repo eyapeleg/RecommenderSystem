d6157d81a424be44674a9e99e76bccac54d144d1
yaelbrumer <yael.brumer@gmail.com>
2016-06-01 09:29:33 +0300

    Bug fix

--- SimilarityMethods/JaccardMethod.cs
+++ SimilarityMethods/JaccardMethod.cs
@@ -9,9 +9,15 @@ namespace RecommenderSystem
     {
         public double calculateSimilarity(User u1, User u2, List<string> intersectList)
         {
+            double intersectCount = intersectList.Count();
+
+            if (intersectCount == 0)
+            {
+                return 0;
+            }
+
             double u1Count = u1.GetRatedItems().Count();
             double u2Count = u2.GetRatedItems().Count();
-            double intersectCount = intersectList.Count();
 
             return intersectCount / (u1Count + u2Count);
         }
